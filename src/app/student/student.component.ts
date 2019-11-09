94df6e3c562ccaf3299a2efc3619ba57945b7995
shovon487 <mislam6789@gmail.com>
2019-11-09 13:40:07 +0600

    POST Delete method Added

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { Alert } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-student',
@@ -14,9 +15,22 @@ export class StudentComponent implements OnInit {
 
   ngOnInit() {
     this.h.get('https://jsonplaceholder.typicode.com/users')
-    .subscribe(r=> {
-      this.userList=r.json();
+    .subscribe(response=> {
+      this.userList=response.json();
       //console.log(r.json());
+    })
+  }
+  AddStudent(inputName){
+    let student ={name: inputName.value };
+    this.h.post('https://jsonplaceholder.typicode.com/users',student)
+    .subscribe(response=> {
+      this.userList.splice(0,0,student);
+    })
+  }
+  DeleteStudent(std){
+    this.h.delete('https://jsonplaceholder.typicode.com/users'+"/"+std.id)
+    .subscribe(response=> {
+      alert("Delete Respone Received "+ response);
     })
   }
 
